9d40acda4baaf001afbbd7bbb758e9877ab35eae
Marcoslarraza <markkusito@gmail.com>
2022-09-20 19:19:37 -0300

    contextdesafio

--- src/containers/cartContainer/index.jsx
+++ src/containers/cartContainer/index.jsx
@@ -14,22 +14,21 @@ const Cart = () => {
       </div>
       
       <div className="container">
+    <div className="row">
+       
+        <main id="items" className="col-sm-8 row"></main>
         
-        <div className="row">
-          
-            <main id="items" className="col-sm-10 row"></main>
+        <aside className="col-sm-8">
+            <h2>Carrito</h2>
             
-              <aside clasNames="col-sm-10">
-                  <h2>Carrito</h2>
-                  
-                  <ul id="carrito" className="list-group"></ul>
-                  <hr/>
-                  
-                  <p className="text-right">Total: <span id="total"></span>&euro;</p>
-                  <button id="boton-vaciar" className="btn btn-danger">Vaciar</button>
-              </aside>
-        </div>
-      </div>
+            <ul id="carrito" className="list-group"></ul>
+            <hr/>
+            
+            <p className="text-right">Total: <span id="total"></span>&euro;</p>
+            <button id="boton-vaciar" className="btn btn-danger">Vaciar</button>
+        </aside>
+    </div>
+</div>
 
 
     </div>
