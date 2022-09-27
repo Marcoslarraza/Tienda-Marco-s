3836d1a3f1ff5f9e2c5a93b74e3dd779d9ec27ef
Marcoslarraza <markkusito@gmail.com>
2022-09-27 02:31:42 -0300

    cartView e Item Collection

--- src/containers/cartContainer/index.jsx
+++ src/containers/cartContainer/index.jsx
@@ -11,62 +11,69 @@ const Cart = () => {
 
   
   
-  const {cart, removeItem, clearCart} = useContext(Shop);
-  if (cart.length === 0) {
-    return(
-      <>
-      <div className="alert alert-primary" role="alert">
-  No hay elementos en el carrito
-  </div>
-  <Link to='/' className="nav-link active" aria-current="page" >{<button className="btn btn-success" >Volver a la tienda</button>}</Link>
+  const {cart, removeItem, clearCart, totalPrice} = useContext(Shop);
 
-      </>
-    );
-  }
+
+      if (cart.length === 0) {
+        return(
+          <>
+          <div className="alert alert-primary" role="alert">
+      No hay elementos en el carrito
+      </div>
+      <Link to='/' className="nav-link active" aria-current="page" >{<button className="btn btn-success" >Volver a la tienda</button>}</Link>
+
+          </>
+        );
+      }
+
+
   const renderImage = (image) => {
-    return(
-      <img src={image.value} alt="cart-product" style={{height: '120px'}}></img>
-    )
-  }
+
+        return(
+          <img src={image.value} alt="cart-product" style={{height: '120px'}}></img>
+        )
+      }
+
 
   const renderRemoveButton = (item) => {
-    const product = item.value
-    return (
-      <Button onClick={()=> removeItem(product)} variant="contained" color="error">
-        Eliminar
-      </Button>
-    )
-  }
+        const product = item.value
+        return (
+          <Button onClick={()=> removeItem(product)} variant="contained" color="error">
+            Eliminar
+          </Button>
+        )
+      }
+
 
   const columns = [
-    { field: 'image', headerName: 'Imagen', width: 250, renderCell: renderImage},
-    { field: 'title', headerName: 'Producto', width: 450 },
-    { field: 'quantity', headerName: 'Cantidad', width: 80 },
-    {field: 'price', headerName: 'Subtotal', width: 100},
-    {
-      field: 'remove',
-      headerName: 'Eliminar',
-      renderCell: renderRemoveButton,
-      width: 120,
-    },
-    
-    
-  ];
+        { field: 'image', headerName: 'Imagen', width: 250, renderCell: renderImage},
+        { field: 'title', headerName: 'Producto', width: 450 },
+        { field: 'quantity', headerName: 'Cantidad', width: 80 },
+        {field: 'price', headerName: 'Subtotal', width: 100},
+        {
+          field: 'remove',
+          headerName: 'Eliminar',
+          renderCell: renderRemoveButton,
+          width: 120,
+        },
+        
+        
+      ];
 
   //Vamos a asignar un array con cada fila de la tabla, utilizando el cart
   const filas = []
-  cart.forEach(item => {
-    filas.push({
-      id: item.id,
-      image: item.image,
-      title: item.title,
-      quantity: item.quantity,
-      remove: item,
-      price: item.price * item.quantity
-     
-    
-    })
-  })
+      cart.forEach(item => {
+        filas.push({
+          id: item.id,
+          image: item.image,
+          title: item.title,
+          quantity: item.quantity,
+          remove: item,
+          price: item.price * item.quantity
+        
+        
+        })
+      })
   
   
   return (
@@ -80,12 +87,26 @@ const Cart = () => {
         
         
       />     
+             <>
+             <div className="alert alert-primary" role="alert">
+                 Resumen de Compra
+                 <h3 className="cart-view-total">Total: $ { totalPrice() }</h3>
+              </div>
+               
+                
+                
+                
+
+             </>
              <Button onClick={clearCart} className="btn btn-danger" >Vaciar carrito</Button> 
             <Link to='/' className="nav-link active" aria-current="page" >{<button className="btn btn-success" >Volver a la tienda</button>}</Link>
             <Link to='/' className="nav-link active" aria-current="page" >{<button className="btn btn-info" >Terminar la compra</button>}</Link>
         
 
+           
+
     </div>
+      
   );
 }
 
